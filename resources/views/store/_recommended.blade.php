e2151ef03e7adf1b57e601ff11e680dfa7526301
Taruman <taru931@gmail.com>
2015-12-12 05:24:38 -0200

    criando a home da loja e separando as áreas em _partials

---
+++ resources/views/store/_recommended.blade.php
@@ -0,0 +1,34 @@
+<div class="features_items"><!--recommended-->
+    <h2 class="title text-center">Recomendados</h2>
+    @foreach($rProducts as $product)
+    <div class="col-sm-4">
+        <div class="product-image-wrapper">
+            <div class="single-products">
+                <div class="productinfo text-center">
+
+                	@if(count($product->images) > 0)
+                    <img src="{{asset('uploads/'.$product->images->first()->id.'.'.$product->images->first()->extension)}}">
+                    @else
+                    <img src="{{asset('images/no-img.jpg')}}">
+                    @endif
+                    
+                    <h2>R$ {{$product->price}}</h2>
+                    <p>{{$product->name}}</p>
+                    <a href="" class="btn btn-default add-to-cart"><i class="fa fa-crosshairs"></i>Mais detalhes</a>
+
+                    <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Adicionar no carrinho</a>
+                </div>
+                <div class="product-overlay">
+                    <div class="overlay-content">
+                        <h2>R$ {{$product->price}}</h2>
+                        <p>{{$product->name}}</p>
+                        <a href="" class="btn btn-default add-to-cart"><i class="fa fa-crosshairs"></i>Mais detalhes</a>
+
+                        <a href="" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Adicionar no carrinho</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div><!--recommended-->
